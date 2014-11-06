99dabdec59245faded149b9e01e070be60b54e63
Cypress Frankenfeld <iamcypress@gmail.com>
2014-11-05 20:03:22 -0500

    sawtoothchrip

---
+++ code/ch3_excercise1.py
@@ -0,0 +1,42 @@
+from thinkdsp import *
+import thinkplot
+
+
+class SawtoothChirp(Chirp):
+
+    def _evaluate(self, ts, freqs):
+        """Helper function that evaluates the signal.
+
+        ts: float array of times
+        freqs: float array of frequencies during each interval
+        """
+        dts = numpy.diff(ts)
+        # dps = PI2 * freqs * dts
+        dcycles = freqs * dts
+        cycles = numpy.cumsum(dcycles)
+        cycles = numpy.insert(cycles, 0, 0)
+        frac, _ = numpy.modf(cycles)
+        ys = normalize(unbias(frac), self.amp)
+        return ys
+
+    def evaluate_sawtooth(self, ts, freq):
+        """Evaluates the signal at the given times.
+
+        ts: float array of times
+
+        returns: float wave array
+        """
+        cycles = freq * ts + self.offset / PI2
+        frac, _ = numpy.modf(cycles)
+        ys = normalize(unbias(frac), self.amp)
+        return ys
+
+if __name__ == "__main__":
+    sawtooth = SawtoothSignal()
+    sawtooth_wave = sawtooth.make_wave()
+    chirp = SawtoothChirp()
+    wave = chirp.make_wave(duration=1, framerate=10000)
+    segment = wave.segment(duration=0.02)
+    segment.plot()
+    wave.play()
+    thinkplot.show()
